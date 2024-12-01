f1c2e4d43aac7df3fed6cf0ed10a1ff84b9465c9
RaulGM3 <apaladario@gmail.com>
2024-12-01 13:38:43 +0100

    libTest added and ready to operate.

---
+++ projects/lib-test/src/lib/lib-test.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-libTest',
+  imports: [],
+  template: `
+    <p>
+      lib-test works!
+    </p>
+  `,
+  styles: ``
+})
+export class LibTestComponent {
+
+}
