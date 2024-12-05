f6ee36768b47b36afd21945ba96f7982339b0504
RaulGM3 <apaladario@gmail.com>
2024-12-05 11:07:01 +0100

    Change with :not pseudo class

--- src/app/youtube-button/youtube-button.component.ts
+++ src/app/youtube-button/youtube-button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[yt-button]',
+  selector: 'button[yt-button]:not([disabled=false])',
   imports: [],
   templateUrl: './youtube-button.component.html',
   styleUrl: './youtube-button.component.sass'
