79365dfa9528be2240073cd6c6bddb6e5f0f71c2
RaulGM3 <apaladario@gmail.com>
2024-12-05 10:51:27 +0100

    Trial with attribute selector for a component.

---
+++ src/app/youtube-button/youtube-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button[yt-button]',
+  imports: [],
+  templateUrl: './youtube-button.component.html',
+  styleUrl: './youtube-button.component.sass'
+})
+export class YoutubeButtonComponent {
+
+}
