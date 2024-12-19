c138ad7e51a382ce183413242a67b89359a37408
RaulGM3 <apaladario@gmail.com>
2024-12-19 09:23:22 +0100

    Adding params to pipe playground

--- src/app/kebab-case.pipe.ts
+++ src/app/kebab-case.pipe.ts
@@ -6,8 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class KebabCasePipe implements PipeTransform {
 
   transform(value: string, ...args: unknown[]): unknown {
+    args.forEach(arg => {
+      console.log(arg);
+    })
     return value.toLowerCase().replace(/ /g, '-');
 
   }
-
 }
